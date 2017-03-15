b2314a0ecc49e84a4f99d99fc2770a18a37e6618
T. Jääskeläinen <timo.a.jaaskelainen@helsinki.fi>
2017-03-15 10:41:10 +0200

    Start

---
+++ app/src/main/java/com/tsh/dis/MainActivity.java
@@ -0,0 +1,58 @@
+package com.tsh.dis;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity {
+    protected File extStorageAppBasePath;
+
+    protected File extStorageAppCachePath;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        ;
+
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                return true;
+            case R.id.
+        default:
+        return super.onOptionsItemSelected(item);
+        }
+    }
+}
